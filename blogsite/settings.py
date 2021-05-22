9daf9e21154f8524d8f773cecc765cdebf621a30
jaywomack <womackjw@hotmail.com>
2021-05-21 17:44:27 -0700

    Adding four columns for articles to stack vertically

--- blogsite/settings.py
+++ blogsite/settings.py
@@ -105,7 +105,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "/blog/static/images")
 ]
-print(os.path.join(BASE_DIR))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
